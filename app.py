9a480633fe4e5f95fb2bc687885c7e4651aaa648
Đinh Hoài Nam <231124029217@due.udn.vn>
2024-11-05 14:09:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from flask import Flask, request, jsonify, render_template
-
+import os
 
 app = Flask(__name__)
 
